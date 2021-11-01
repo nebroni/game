f63b5815b21f0bd419f636efc67a9f064a1cd14f
Vlad <grinkivvlad2@gmail.com>
2021-11-01 20:56:34 +0200

    add ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,21 +1,32 @@
 import sys
 import pygame
+from settings import Settings
+from ship import Ship
 
 
 class AlienInvasion:
 	def __init__(self):
 		pygame.init()
-		self.screen = pygame.display.set_mode((1550, 800))
+		self.settings = Settings()
+		self.screen = pygame.display.set_mode(
+			(self.settings.screen_width, self.settings.screen_height))
 		pygame.display.set_caption('Alien Invasion')
+		self.ship = Ship(self)
+
+	def _check_events(self):
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				sys.exit()
+
+	def _update_screen(self):
+		self.screen.fill(self.settings.bg_color)
+		self.ship.blitme()
+		pygame.display.flip()
 
 	def run_game(self):
 		while True:
-			for event in pygame.event.get():
-				if event.type == pygame.QUIT:
-					sys.exit()
-
-			pygame.display.flip()
-
+			self._check_events()
+			self._update_screen()
 
 if __name__ == '__main__':
 	AlienInvasion().run_game()
